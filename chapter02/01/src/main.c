64a3bd1191fa676c5069ff0205b601084e0cde0d
Patrick Mims <patrickkevinmims@gmail.com>
2021-12-01 11:41:00 -0800

    latest' '

--- chapter02/01/src/main.c
+++ chapter02/01/src/main.c
@@ -1,10 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define INDEX 20
+
+// utility function
+void errorCheck(FILE *e)
+{
+    if(e == NULL)
+    {
+      perror("Error");
+      exit(EXIT_FAILURE);
+    }
+}
+
+void readResults(FILE *r)
+{
+  r = fopen("results.txt", "r");
+
+  errorCheck(r);
+
+  printf("things are ok...");
+
+  fclose(r);
+}
+
+void readArtists()
+{
+    FILE *fpArtist = NULL;
+
+    fpArtist = fopen("fpArtist", "r");
+
+    if(fpArtist == NULL)
+    {
+        perror("Error");
+        exit(EXIT_FAILURE);
+    }
+
+    fclose(fpArtist);
+}
+
+void voteForArtist(FILE *a)
+{
+
+}
+
 int main(void)
 {
+  char buffer[INDEX];
+
   FILE *fp = NULL;
-  char buffer[100];
 
   fp = fopen("results.txt", "r");
 
@@ -14,12 +58,16 @@ int main(void)
     exit(EXIT_FAILURE);
   }
 
+  /* read the first column */
   while(fscanf(fp, "%s", buffer) == 1)
   {
     printf("%s\n", buffer);
   }
 
   fclose(fp);
+
+  readResults(fp);
+  voteForArtist(fp);
 
   return 0;
 }

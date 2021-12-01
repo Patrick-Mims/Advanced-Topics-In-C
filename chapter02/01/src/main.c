cc8c02095dd7f574fca0c5cdf7f92b411fddcb02
Patrick Mims <patrickkevinmims@gmail.com>
2021-12-01 11:08:35 -0800

    latest

--- chapter02/01/src/main.c
+++ chapter02/01/src/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-  printf("Hello\n");
+  FILE *fp = NULL;
+  char buffer[100];
+
+  fp = fopen("results.txt", "r");
+
+  if(fp == NULL)
+  {
+    perror("Error");
+    exit(EXIT_FAILURE);
+  }
+
+  while(fscanf(fp,"%*s") == 1)
+  {
+    printf("%s\n", buffer);
+  }
+
+  fclose(fp);
+
   return 0;
 }

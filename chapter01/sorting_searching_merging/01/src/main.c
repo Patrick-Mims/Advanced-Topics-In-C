ad4a6ffc104683ec0df60246ce040a7556328274
Patrick Mims <patrickkevinmims@gmail.com>
2021-12-30 19:58:06 -0800

    implent sorting algorithm

--- chapter01/sorting_searching_merging/01/src/main.c
+++ chapter01/sorting_searching_merging/01/src/main.c
@@ -23,6 +23,10 @@ typedef struct
   int vote;
 } vote_t; /* log the vote to a structure, and write it to a file */
 
+void insertionSort(int *list[], int lo, int hi)
+{
+}
+
 void logVote(vote_t lv)
 {
   printf("My Vote: %d\n", lv.vote);
@@ -85,9 +89,9 @@ void readArtist(vote_t *v, char *buff)
  * write a struct (int, int), that will hold the index and count data.
  * */
 
-void display(char *list[], int index, int count)
+void display(char *list, int index, int count)
 {
-  printf("%s - %d\n", list[index], count);
+  printf("%s - %d\n", list, count);
 
   FILE *fp = NULL;
 
@@ -99,28 +103,37 @@ void display(char *list[], int index, int count)
     exit(EXIT_FAILURE);
   }
 
-  fprintf(fp, "%s - %d\n", list[index], count);
+  fprintf(fp, "%s - %d\n", list, count);
 
   fclose(fp);
 }
 
 int main(void)
 {
+  if (remove("results.txt"))
+  {
+    printf("\n\tSuccess: results.txt removed!\n");
+    return 0;
+  }
+
   char buffer[INDEX];
+
+  // TODO: Try to get rid of these...struct?
   int cnt = 0;
-  int five = 0;
-  int four = 0;
-  int j;
-  int one = 0;
-  int three = 0;
-  int two = 0;
-  int vote = 0;
-  int zero = 0;
+  static int five = 0;
+  static int four = 0;
+  static int j;
+  static int one = 0;
+  static int three = 0;
+  static int two = 0;
+  static int vote = 0;
+  static int zero = 0;
 
   vote_t v[6];
 
+  /* populate vote_t v */
   readArtist(v, buffer);
-  /* display the names in the structure */
+
   for (j = 0; j < 6; j++)
   {
     printf("%d. %s\n", j, v[j].name);
@@ -140,39 +153,38 @@ int main(void)
 
     // TODO: pass the structure to the display function
 
-    /*
-        switch (vote)
-        {
-        case 0:
-          zero++;
-          display(artists, vote, zero);
-          break;
-        case 1:
-          one++;
-          display(artists, vote, one);
-          break;
-        case 2:
-          two++;
-          display(artists, vote, two);
-          break;
-        case 3:
-          three++;
-          display(artists, vote, three);
-          break;
-        case 4:
-          four++;
-          display(artists, vote, four);
-          break;
-        case 5:
-          five++;
-          display(artists, vote, five);
-          break;
-        default:
-          printf("Nothing Here...");
-        }
-        */
+    switch (vote)
+    {
+    case 0:
+      zero = zero + 1;
+      display(v[0].name, vote, zero);
+      break;
+    case 1:
+      one = one + 1;
+      display(v[1].name, vote, one);
+      break;
+    case 2:
+      two = two + 1;
+      display(v[2].name, vote, two);
+      break;
+    case 3:
+      three = three + 1;
+      display(v[3].name, vote, three);
+      break;
+    case 4:
+      four = four + 1;
+      display(v[4].name, vote, four);
+      break;
+    case 5:
+      five = five + 1;
+      display(v[5].name, vote, five);
+      break;
+    default:
+      printf("Nothing Here...");
+    }
 
     cnt = cnt + 1;
+
   } while (cnt < 8);
 
   return 0;
